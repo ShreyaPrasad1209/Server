3f563c0aaeaad7becd3e4456848f46bb9742e9a0
Shreya Prasad <shreyapd06@gmail.com>
2019-07-02 23:55:34 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-dbPassword = 'mongodb+srv://shreya:'+ encodeURIComponent('K#$29go&$') + 'project.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://username:'+ encodeURIComponent<'password'> + 'project.mongodb.net/test?retryWrites=true';
 
 module.exports = {
-    mongoURI: 'mongodb+srv://shreya:K#$29go&$@project-r5xnf.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://username:password@project-r5xnf.mongodb.net/test?retryWrites=true&w=majority'
 };
